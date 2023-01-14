2161080593af6d543b7d00e2e9916dbacb95f096
aidan <apdelany1@gmail.com>
2023-01-13 22:59:17 -0500

    added a way to generate a random rock paper or scissors with the Math random function, I also added a prompt for a user to input their own choice

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-const rps = Math.floor(Math.random() * 3 + 1)
+function getComputerChoice () {
 
+    let rps = Math.floor(Math.random() * 3 + 1)
+    let challenge;
+
+    if (rps === 1) {
+        challenge = "rock";
+    } else if (rps === 2) {
+        challenge = "paper";
+    } else {
+        challenge = "scissors";
+    }
+
+    return challenge;
+}
+
+
+const computerSelection = getComputerChoice()
+const playerSelection = prompt("Rock, Paper or Scissors?")
+
+console.log(computerSelection.toUpperCase());
+console.log(playerSelection.toUpperCase());
