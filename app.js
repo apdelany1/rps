922bc0f757a424216e889c7a790de4d1456f3f17
aidan <apdelany1@gmail.com>
2023-01-20 00:51:01 -0500

    refactored lots of logic, it is still ugly, but I am tired and I should sleep. I need to fix the fact that both people can still reach 5, it should block all attempts to add more scores to the board. added a set timeout to reset, and added messages depending on who won, first DOM project

--- app.js
+++ app.js
@@ -4,22 +4,39 @@ let computerScore = 0;
 
 
 const rock = document.querySelector("#rockBtn");
-const paper = document.querySelector("#paperBtn")
-const scissors = document.querySelector("#scissorsBtn") 
+const paper = document.querySelector("#paperBtn");
+const scissors = document.querySelector("#scissorsBtn");
+
+const cscore = document.querySelector("#computerscore")
+const hscore = document.querySelector("#humanscore")
+
+const msg = document.querySelector("#message")
+
+
+
 
 rock.addEventListener('click', function (e) {
+    if (computerScore == 5 || computerScore == 5) {
+        return 0;
+    }
     let computerSelection = getComputerChoice().toUpperCase()
     let humanSelection = "ROCK"
     playRound(humanSelection, computerSelection);
 });
 
 paper.addEventListener('click', function (e) {
+    if (computerScore == 5 || computerScore ==5) {
+        return 0;
+    }
     let computerSelection = getComputerChoice().toUpperCase()
     let humanSelection = "PAPER"
     playRound(humanSelection, computerSelection);
 });
 
 scissors.addEventListener('click', function (e) {
+    if (computerScore == 5 || computerScore == 5) {
+        return 0;
+    }
     let computerSelection = getComputerChoice().toUpperCase()
     let humanSelection = "SCISSORS"
     playRound(humanSelection, computerSelection);
@@ -28,31 +45,30 @@ scissors.addEventListener('click', function (e) {
 
 
 
-function game() {
+// function game() {
 
-    for (let i = 0; i < 5; i++) {
+//     for (let i = 0; i < 5; i++) {
 
-        let computerSelection = getComputerChoice().toUpperCase()
-        let playerSelection = prompt("Rock, Paper Scissors: SHOOT!").toUpperCase()
+//         let computerSelection = getComputerChoice().toUpperCase()
+//         let playerSelection = prompt("Rock, Paper Scissors: SHOOT!").toUpperCase()
 
-        let winner = playRound(playerSelection, computerSelection)
+//         let winner = playRound(playerSelection, computerSelection)
 
-        if (winner === 1) {
-            i--;
-        } else if (winner === 2) {
-            computerScore++;
-        } else if (winner === 3) {
-            humanScore++;
-        }
+//         if (winner === 1) {
+//             i--;
+//         } else if (winner === 2) {
+//             computerScore++;
+//         } else if (winner === 3) {
+//             humanScore++;
+//         }
+//     }
 
-    }
-
-    if (humanScore > computerScore) {
-        alert(`You Won ${humanScore} to ${computerScore}! Congrats!`)
-    } else {
-        alert(`You LOST ${computerScore} to ${humanScore}! slickhead!`)
-    }
-}
+//     if (humanScore > computerScore) {
+//         alert(`You Won ${humanScore} to ${computerScore}! Congrats!`)
+//     } else {
+//         alert(`You LOST ${computerScore} to ${humanScore}! slickhead!`)
+//     }
+// }
 
 
 
@@ -60,37 +76,64 @@ function game() {
 
 
 function playRound(playerSelection, computerSelection) {
-    
+
     if (playerSelection.localeCompare(computerSelection) === 0) {
         console.log(`${computerSelection} ties ${playerSelection}`);
-        return 1
+        return 0;
     }
 
     if (playerSelection ==="ROCK") {
         if (computerSelection.localeCompare("PAPER") === 0) {
-            console.log("YOU LOSE! PAPER BEATS ROCK")
-            return 2
+            msg.innerText = "YOU LOSE! PAPER BEATS ROCK"
+            computerScore++;
+            cscore.innerText = computerScore
         } else {
-            console.log("YOU WIN! ROCK BEATS SCISSORS")
-            return 3
+            msg.innerText = "YOU WIN! ROCK BEATS SCISSORS"
+            humanScore++;
+            hscore.innerText = humanScore
         }
     } else if (playerSelection === "PAPER") {
         if (computerSelection.localeCompare("SCISSORS") === 0) {
-            console.log("YOU LOSE!! SCISSORS BEATS PAPER")
-            return 2
+            msg.innerText = "YOU LOSE! SCISSORS BEATS PAPER"
+            computerScore++;
+            cscore.innerText = computerScore
         } else {
-            console.log("YOU WIN! PAPER BEATS ROCK!")
-            return 3
+            msg.innerText = "YOU WIN! PAPER BEATS ROCK"
+            humanScore++;
+            hscore.innerText = humanScore
         }
     } else if (playerSelection === "SCISSORS") {
         if (computerSelection.localeCompare("ROCK") === 0) {
-            console.log("YOU LOSE!! ROCK BEATS SCISSORS")
-            return 2
+            msg.innerText = "YOU LOSE! ROCK BEATS SCISSORS"
+            computerScore++;
+            cscore.innerText = computerScore
         } else {
-            console.log("YOU WIN!! SCISSORS BEATS PAPER!!")
-            return 3
+            msg.innerText = "YOU WIN! SCISSORS BEATS PAPER"
+            humanScore++;
+            hscore.innerText = humanScore
         }
     } 
+
+    if (humanScore == 5) {
+        msg.innerText = "congrats human, you win"
+        setTimeout(() => {
+            humanScore = 0;
+            computerScore = 0;
+            hscore.innerText = humanScore
+            cscore.innerText = computerScore
+            msg.innerText = ""
+        },3000)
+
+    } else if (computerScore == 5) {
+        msg.innerText = "sorry human, you lose"
+        setTimeout(() => {
+            humanScore = 0;
+            computerScore = 0;
+            hscore.innerText = humanScore
+            cscore.innerText = computerScore
+            msg.innerText = ""
+        },3000)
+    }
 }
 
 
